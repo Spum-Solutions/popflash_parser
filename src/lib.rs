a84bf2abacc0deb0fa9fefcd5cce8aeec0a5deec
nectariner <fredcook789@protonmail.com>
2022-07-05 19:35:58 +0100

    changed public facing docs

--- src/lib.rs
+++ src/lib.rs
@@ -1,9 +1,10 @@
+#![deny(missing_docs)]
+
 //! `popflash_parser` is a crate to parse and translate match information from <https://popflash.site/> This tool is currently working as of February 27th 2022, although other similar tools have been borked when `popflash` has changed it's web page structure so take caution when using.
 //!
-//! Methods are used to get the match data from a url or match ID, the are functionally equivalent to the end user only differing in what is used to call the function
+//! Methods are used to get the match data from a url or match ID, they are functionally equivalent to the end user, only differing in what is used to call the function (a url, or match ID)
 //!
-//!
-//! ```
+//! ``` rust
 //! # use popflash_parser::*;
 //! ##[tokio::test]
 //! # async fn test() {
@@ -36,13 +37,22 @@
 //! # }
 //! ```
 
+#[doc(hidden)]
+const EXAMPLE_MATCH_URL: &str = "https://popflash.site/match/1281644";
+
+#[doc(hidden)]
+const EXAMPLE_MATCH_ID: usize = 1281644;
+
 use r#match::Match;
 
 mod player_stats;
 mod team;
+
+#[doc(hidden)]
 pub mod utility;
 
 // match is a rust keyword so need to be done like this
+#[doc(hidden)]
 pub mod r#match;
 
 /// Retrieve match information from a match id such as `1281644`
@@ -60,14 +70,12 @@ pub async fn match_from_url(url: &str) -> Result<Match, Box<dyn std::error::Erro
 #[cfg(test)]
 mod tests {
     use super::*;
-    const EXAMPLE_GAME_URL: &str = "https://popflash.site/match/1281644";
-    const EXAMPLE_GAME_ID: usize = 1281644;
     mod match_from_id {
         use super::*;
         #[tokio::test]
+        // valid test as this is to check that popflash hasn't changed it's output
         async fn valid_popflash_id_1() {
-            let body = utility::get_body_from_id(EXAMPLE_GAME_ID).await.unwrap();
-            todo!()
+            assert!(match_from_id(EXAMPLE_MATCH_ID).await.is_ok());
         }
 
         #[test]
@@ -77,8 +85,11 @@ mod tests {
     }
 
     mod match_from_url {
-        #[test]
-        fn valid_popflash_url_1() {
+
+        use super::*;
+        #[tokio::test]
+        async fn valid_popflash_url_1() {
+            let body = match_from_url(EXAMPLE_MATCH_URL).await.unwrap();
             todo!()
         }
 
--- src/utility.rs
+++ src/utility.rs
@@ -1,15 +1,18 @@
-use reqwest::Error;
 use select::document::Document;
 
 /// This makes use of the function `get_body_from_url` function in the same module by converting the id to a valid url, this is just for quality of life reasons
-pub async fn get_body_from_id(match_id: usize) -> Result<select::document::Document, Error> {
+pub async fn get_body_from_id(
+    match_id: usize,
+) -> Result<select::document::Document, Box<dyn std::error::Error>> {
     let url = format!("https://popflash.site/match/{}", match_id);
     let result = get_body_from_url(url.as_ref()).await?;
 
     Ok(result)
 }
 
-pub async fn get_body_from_url(match_url: &str) -> Result<select::document::Document, Error> {
+pub async fn get_body_from_url(
+    match_url: &str,
+) -> Result<select::document::Document, Box<dyn std::error::Error>> {
     let body = reqwest::get(match_url).await?.text().await?;
 
     let document = Document::from(body.as_ref());
