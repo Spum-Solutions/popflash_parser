befff6fd1c57de517e0826215717575b68afe487
nectariner <fredcook789@protonmail.com>
2022-07-06 12:15:15 +0100

    added functionality for getting datetime of game and map name

--- src/match.rs
+++ src/match.rs
@@ -1,22 +1,20 @@
 use super::team::Team;
-use select::{document::Document, predicate::Attr};
+use chrono::prelude::*;
 
-use select::predicate::Class;
-use select::predicate::Name;
+use select::document::Document;
+use select::predicate::Text;
+use select::predicate::{Attr, Class, Name, Predicate};
 
 use serde_derive::{Deserialize, Serialize};
 
 // Matches have unique match IDs and therefore can implement Eq
 #[derive(Serialize, Deserialize, Debug)]
 pub struct Match {
+    // TODO: other game modes won't work because e.g. no bomb plants in hostage mode so parsing will fail
     id: String,
-
-    // TODO:
-    // date
-    // time
-    // type
-    // map
-    // link to demo
+    datetime: DateTime<Utc>,
+    // game_type (only one I can see is scrim but there could be other types potentially)
+    map: String,
     teams: Vec<Team>,
 }
 
@@ -33,7 +31,63 @@ impl Match {
         Self {
             id: Self::get_match_id(document),
             teams: Match::get_teams(document),
+            datetime: Self::popflash_time_to_utc(document).unwrap(),
+            map: Self::get_map_name(document),
         }
+    }
+
+    fn popflash_time_to_utc(
+        document: &Document,
+    ) -> Result<DateTime<Utc>, Box<dyn std::error::Error>> {
+        // TODO: this needs a major refactor
+        // example popflash date looks like "06/02/2022 AT 20:05:43"
+
+        let mut date = document.find(Name("h2")).into_iter().next().unwrap().text();
+        let mut time = date.clone();
+
+        date.replace_range((0..date.find("ON ").unwrap() + 3), "");
+        date.replace_range((date.find(' ').unwrap()..), "");
+
+        time.replace_range((0..time.find("AT ").unwrap() + 3), "");
+        time.replace_range((time.find(' ').unwrap()..), "");
+
+        let date_iter = date.split('/');
+        let mut time_iter = time.split(':');
+
+        let dates = date_iter.collect::<Vec<_>>();
+
+        let datetime = Utc
+            .ymd(
+                dates[2].parse::<i32>().unwrap(),
+                dates[0].parse::<u32>().unwrap(),
+                dates[1].parse::<u32>().unwrap(),
+            )
+            .and_hms(
+                time_iter.next().unwrap().parse::<u32>().unwrap(),
+                time_iter.next().unwrap().parse::<u32>().unwrap(),
+                time_iter.next().unwrap().parse::<u32>().unwrap(),
+            );
+
+        Ok(datetime)
+    }
+
+    fn get_map_name(document: &Document) -> String {
+        let lines = document.nth(0).unwrap().html();
+        let new_lines: Vec<String> = lines
+            .lines()
+            .collect::<Vec<&str>>()
+            .into_iter()
+            .map(|element| element.to_string())
+            .collect();
+        let map_name = new_lines
+            .iter()
+            .position(|line| line.contains("score score-2"))
+            .unwrap()
+            + 6;
+        let map_name = new_lines[map_name].clone();
+        println!("\n\n\n\n\n{map_name}");
+
+        map_name
     }
 
     fn get_teams(document: &Document) -> Vec<Team> {
